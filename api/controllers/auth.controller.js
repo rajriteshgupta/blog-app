358235f953d7829ea32b6f79d01f9c9c3070a8ba
Ritesh Raj <rajriteshkatihar@gmail.com>
2024-06-07 11:34:49 +0530

    Signin page created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,12 +52,13 @@ export const signin = async (req, res, next) => {
         }
 
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password: pass, ...rest} = validUser._doc;
         res
           .status(200)
           .cookie("access_token", token, {
             httpOnly: true,
           })
-          .json(validUser);
+          .json(rest);
     } catch (error) {
         next(error);
     }
