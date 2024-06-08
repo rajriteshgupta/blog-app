ffa7580c693f87a1d9128a4654506848008c7330
Ritesh Raj <rajriteshkatihar@gmail.com>
2024-06-08 23:41:40 +0530

    Private route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -19,7 +20,9 @@ export default function App() {
       <Route path='/about' element={<About />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route element={<PrivateRoute />}>
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Route>
       <Route path='/projects' element={<Projects />} />
     </Routes>
     <Footer/>
