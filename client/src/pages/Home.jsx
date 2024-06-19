9f74d4dbba98414475a7d594328296685f8fb6e3
Ritesh Raj <rajriteshkatihar@gmail.com>
2024-06-20 02:22:30 +0530

    Home page created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+
+import CallToAction from '../components/CallToAction';
+import PostCard from '../components/PostCard';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch('/api/post/getposts?limit=9');
+        const data = await res.json();
+        if(res.ok){
+          setPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchPosts();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="flex flex-col gap-6 px-3 p-28 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">
+          Welcome to&nbsp;
+          <span className="self-center whitespace-nowrap lg:text-5xl dark:text-white"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Ritesh's
+            </span>
+            Blog
+          </span>
+        </h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Here you'll find variety of arcticles and tutorials on topics such as
+          web development, software engineering, programming languages and
+          latest technology trends.
+        </p>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700 lg:px-28">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto flex flex-col p-3 gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-5">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
