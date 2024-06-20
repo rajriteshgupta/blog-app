603041f8f80feb8a11fe9abcecb3e8e7796eb062
Ritesh Raj <rajriteshkatihar@gmail.com>
2024-06-21 00:30:14 +0530

    Added pre-installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
@@ -16,6 +17,8 @@ mongoose.connect(process.env.MONGO_DB).then(() => {
     console.error(err);
 })
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -29,6 +32,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
