276a75559ff5dc9fec373f4965e74ee9c0110b9f
Ritesh Raj <rajriteshkatihar@gmail.com>
2024-06-21 00:22:25 +0530

    Projects and about page created

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">About Ritesh's Blog</h1>
+          <div className="tetx-md text-gray-500 flex flex-col gap-6">
+            <p>Coming soon...</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
